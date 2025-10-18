81d7132cb69f9f1fce4e0951d07a96f774297e12
Sinmiloluwa <mofeoluwae@gmail.com>
2025-10-18 09:12:38 +0100

    added config file

---
+++ src/config/dto-mapper.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | DTO Mapper Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This configuration file can be used to customize default behaviors
+    | for your data transfer object (DTO) mappings.
+    |
+    */
+
+    'strict' => true,
+
+];
