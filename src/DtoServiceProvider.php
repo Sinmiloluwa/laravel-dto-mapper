4e870366ba6dc3f0402cb92bc3d68a58be29eede
Sinmiloluwa <mofeoluwae@gmail.com>
2025-10-18 09:31:13 +0100

    updated provider

--- src/DtoServiceProvider.php
+++ src/DtoServiceProvider.php
@@ -8,15 +8,11 @@ class DtoServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/dto-mapper.php' => config_path('dto-mapper.php'),
-            ], 'config');
-        }
+        //
     }
 
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/dto-mapper.php', 'dto-mapper');
+        //
     }
 }
